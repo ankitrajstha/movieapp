ac729faad606d47a1d934e03a004e316f3926d17
Ankit Shrestha <ankitrajstha>
2024-06-16 14:49:53 +0545

    feat: add assets,header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <main>
+      <Header />
       <Outlet />
+      <Footer />
     </main>
   );
 }
