a6c82bc4d1a601720d3c41bdcb2f8f81f10bd92f
Ankit Shrestha <ankitrajstha>
2024-06-17 15:06:04 +0545

    feat: add api bearer and trending data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MobileNavigation from "./components/MobileNavigation";
+import axios from "axios";
 
 export default function App() {
+  const fetchTreandingData = async () => {
+    try {
+      const res = await axios.get("/trending/all/week");
+      const data = res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTreandingData();
+  }, []);
   return (
     <>
       <div className="pb-14 lg:pb-0">
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 axios.defaults.headers.common[
   "Authorization"
-] = `Bearer ${process.env.REACT_ACCESS_TOKEN}`;
+] = `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
