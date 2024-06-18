be32fc9b5af8a390658bfddd0eace362814bfc07
Ankit Shrestha <ankitrajstha>
2024-06-18 19:18:58 +0545

    feat: add detail of the movie

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router";
 import useFetchDetails from "../hooks/useFetchDetail";
 import { useSelector } from "react-redux";
+import { Divider } from "../components/Divider";
 
 const DetailsPage = () => {
   const params = useParams();
@@ -11,9 +12,15 @@ const DetailsPage = () => {
   const { data: castData } = useFetchDetails(
     `/${params?.explore}/${params?.id}/credits`
   );
+
   const duration = data?.runtime
     ? (Number(data.runtime) / 60).toFixed(1).split(".")
     : [0, 0];
+
+  const writer = castData?.crew
+    ?.filter((el) => el?.job === "Writer")
+    ?.map((el) => el?.name)
+    ?.join(", ");
   return (
     <div>
       <div className="w-full h-[280px] relative hidden lg:block">
@@ -21,29 +28,31 @@ const DetailsPage = () => {
           <img
             src={imageURL + data?.backdrop_path}
             className="h-full w-full object-cover"
-            alt="CoverImage"
+            alt="movie_poster"
           />
         </div>
-
         <div className="absolute w-full h-full top-0 bg-gradient-to-t from-neutral-900/90 to-transparent"></div>
       </div>
 
-      <div className="container mx-auto px-3 py-16 lg:py-0 flex flex-col lg:flex-row gap-5 lg:gap-10">
-        <div className="relative mx-auto lg:-mt-28 lg:mx-0 lg: w-fit">
+      <div className="container mx-auto px-3 py-16 lg:py-0 flex flex-col lg:flex-row gap-5 lg:gap-10 ">
+        <div className="relative mx-auto lg:-mt-28 lg:mx-0 w-fit min-w-60">
           <img
             src={imageURL + data?.poster_path}
-            className="h-80 w-80 object-cover rounded"
-            alt="CoverImage"
+            className="h-80 w-60 object-cover rounded"
+            alt="movie_poster"
           />
         </div>
 
         <div>
-          <h2 className="text-2xl font-bold text-white">
+          <h2 className="text-2xl lg:text-4xl font-bold text-white ">
             {data?.title || data?.name}
           </h2>
           <p className="text-neutral-400">{data?.tagline}</p>
-          <div className="flex items-center my-1 gap-3">
-            <p>Rating : {Number(data?.vote_average).toFixed(1)} +</p>
+
+          <Divider />
+
+          <div className="flex items-center gap-3">
+            <p>Rating : {Number(data?.vote_average).toFixed(1)}+</p>
             <span>|</span>
             <p>View : {Number(data?.vote_count)}</p>
             <span>|</span>
@@ -51,15 +60,60 @@ const DetailsPage = () => {
               Duration : {duration[0]}h {duration[1]}m
             </p>
           </div>
-        </div>
 
-        <div>
-          <h3 className="text-xl font-bold text-white mb-1">Overview</h3>
-          <p>{data?.overview}</p>
+          <Divider />
+
           <div>
-            <p>Status : {data?.status}</p>
-            <span>|</span>
-            <p>Release Date : {data?.release_date}</p>
+            <h3 className="text-xl font-bold text-white mb-1">Overview</h3>
+            <p>{data?.overview}</p>
+
+            <Divider />
+            <div className="flex items-center gap-3 my-3 text-center">
+              <p>Staus : {data?.status}</p>
+              <span>|</span>
+              <p>Release Date :{data?.release_date}</p>
+              <span>|</span>
+              <p>Revenue : {Number(data?.revenue)}</p>
+            </div>
+
+            <Divider />
+          </div>
+
+          <div>
+            <p>
+              <span className="text-white">Director</span> :{" "}
+              {castData?.crew[0]?.name}
+            </p>
+
+            <Divider />
+
+            <p>
+              <span className="text-white">Writer : {writer}</span>
+            </p>
+          </div>
+
+          <Divider />
+
+          <h2 className="font-bold text-lg">Cast :</h2>
+          <div className="grid grid-cols-[repeat(auto-fit,96px)] gap-5 my-4">
+            {castData?.cast
+              ?.filter((el) => el?.profile_path)
+              .map((starCast, index) => {
+                return (
+                  <div>
+                    <div>
+                      <img
+                        src={imageURL + starCast?.profile_path}
+                        className="w-24 h-24 object-cover rounded-full"
+                        alt="cast_image"
+                      />
+                    </div>
+                    <p className="font-bold text-center text-sm text-neutral-400">
+                      {starCast?.name}
+                    </p>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
