18531d373de06e2d1ca103e33e516307d05b57cf
Ankit Shrestha <ankitrajstha>
2024-06-18 16:43:45 +0545

    feat: add search functionality for mobile

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import Card from "../components/Card";
 
 const SearchPage = () => {
   const location = useLocation();
   const [data, setData] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const navigate = useNavigate();
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`/search/collection`, {
+      const res = await axios.get(`/search/multi`, {
         params: {
           query: location?.search?.slice(3),
           page: pageNo,
@@ -45,6 +46,14 @@ const SearchPage = () => {
   }, []);
   return (
     <div className="py-16">
+      <div className="lg:hidden my-2 mx-1 sticky top-[70px] z-30">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => navigate(`/search?q=${e.target.value}`)}
+          className="px-4 py-1 text-lg w-full bg-white rounded-full text-neutral-100"
+        />
+      </div>
       <div className="container mx-auto">
         <h3 className="capitalize text-lg text-semibold">Search Results</h3>
         <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6 justify-center lg:justify-start">
